7d2893022fae21c3e835cb83849785f70ac3e2a6
Cassie003 <caseyminu@gmail.com>
2022-04-22 19:15:31 +0300

    test to check if credentials object is created

--- credentials.test.py
+++ credentials.test.py
@@ -8,3 +8,14 @@ class TestCredentials(unittest.TestCase):
         """
         self.new_credentials = Credentials(
             "Telegram", "developerTelegram", "myPass@2003")
+        def test_init(self):
+            """
+           Test to check if the credentials object is created
+        """
+        self.assertEqual(self.new_credentials.account_platform, "Telegram")
+        self.assertEqual(
+            self.new_credentials.user_account_username, "developerTelegram")
+        self.assertEqual(
+            self.new_credentials.user_account_password, "myPass@2003")
+
+
