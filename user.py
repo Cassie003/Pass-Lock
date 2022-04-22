3381f4008603774c37822eb0ae482f19f312d5e7
Cassie003 <caseyminu@gmail.com>
2022-04-22 18:04:02 +0300

    intialize the user

---
+++ user.py
@@ -0,0 +1,13 @@
+class User():
+    
+    user_accounts = []
+
+    def __init__(self, first_name, last_name, username, password):
+        """
+        Initialize the user.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
