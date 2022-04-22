ba3856fc9ed542f74cca487c42202dd91093b535
Cassie003 <caseyminu@gmail.com>
2022-04-22 18:29:40 +0300

    test to check created user exists

--- user_test.py
+++ user_test.py
@@ -8,3 +8,12 @@ class TestUser(unittest.TestCase):
         """
         self.new_user = User("casey", "minu",
                              "developercasey", "k8ddj6m2l")
+    def test_user_created(self):
+        """
+        Test to check if the user object is created
+        """
+        self.assertEqual(self.new_user.first_name, "casey")
+        self.assertEqual(self.new_user.last_name, "minu")
+        self.assertEqual(self.new_user.username, "developercasey")
+        self.assertEqual(self.new_user.password, "k8ddj6m2l")
+
