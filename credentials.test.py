be6f31b0a925abc97bed2ab2447743516ec6ccfd
Cassie003 <caseyminu@gmail.com>
2022-04-22 19:09:31 +0300

    test telegram credentials

--- credentials.test.py
+++ credentials.test.py
@@ -1,2 +1,10 @@
 import unittest
 from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        """
+            Setup method for the test
+        """
+        self.new_credentials = Credentials(
+            "Telegram", "developerTelegram", "myPass@2003")
