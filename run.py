f5a38d31f4c936e757ed78aaa34c0107841c77db
Cassie003 <caseyminu@gmail.com>
2022-04-22 19:43:58 +0300

    ccreate a new user interface

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
 from user import User
 from  credentials import Credentials
 
+def create_new_user(first_name, last_name, username, user_password):  # create a new user
+    '''
+    create a new user
+    '''
+    new_user = User(first_name, last_name, username, user_password)
+    return new_user
+
